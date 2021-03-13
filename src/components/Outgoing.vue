335731833b258662805f5bcefb2bb50a1175c37e
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-03-13 17:14:34 +0000

    base logic for the budget calculation, Outgoing and Salary props, emits added.

---
+++ src/components/Outgoing.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <form action="submit">
+      <label for="outgoing">Outgoing: </label>
+      <input
+        id="outgoing"
+        placeholder="outgoing"
+        type="number"
+        name="outgoing"
+        v-model="outgoingCost"
+        v-on:keyup="emitToSalary"
+      />
+      <button>Add</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["outgoingData"],
+  data: function() {
+    return {
+      outgoingCost: null,
+    };
+  },
+  methods: {
+    emitToSalary(e) {
+      this.outgoingCost = e.target.value;
+      this.$emit("outgoingData", this.outgoingCost);
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/components/Salary.vue
+++ src/components/Salary.vue
@@ -1,20 +1,47 @@
 <template>
   <div class="salary">
+    <div>
       <label for="salary">Income: </label>
-          <input type="number" name="salary" id="salary">
+      <input
+        placeholder="income"
+        type="number"
+        name="salary"
+        id="salary"
+        v-model="salary"
+      />
+    </div>
+    <Outgoing v-on:outgoingData="outgoingSum" />
+
+    <p>Total: {{ total }}</p>
   </div>
 </template>
 
 <script>
+import Outgoing from "./Outgoing.vue";
 export default {
-
-}
+  components: { Outgoing },
+  data: function() {
+    return {
+      salary: null,
+      outgoing: null,
+      total: null,
+    };
+  },
+  methods: {
+    outgoingSum(value) {
+      this.outgoing = value;
+      this.total = parseInt(this.salary) + parseInt(this.outgoing)
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 label {
-    color: white;
+  color: white;
 }
 
+p {
+  color: white;
+}
 </style>
